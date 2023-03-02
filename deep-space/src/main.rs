cd39a42acaffed4300c2b4fa8f0d9c43117273bd
cpadilla <christoferpadilla@gmail.com>
2023-03-02 02:28:46 -0800

    Using Component macro

--- deep-space/src/main.rs
+++ deep-space/src/main.rs
@@ -1,6 +1,15 @@
+use bones::Component;
+
 fn main() {
     println!("Hello, world! {}", bones::add(1, 2));
 
-    pollster::block_on(bones::run());
+    #[derive(Component)]
+    struct Player;
 
+    // bones::Skeleton::new()
+    //      .insert_resources()
+    //      .insert_entities()
+    //      .run()
+
+    pollster::block_on(bones::run());
 }
